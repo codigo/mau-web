cb9d5578ef320b8b3229202c0dd56f7f4cdb8040
Mauricio Mercado <mau@maumercado.com>
2024-07-05 16:19:03 -0700

    Created testimonials carousel

---
+++ src/components/Testimonials/Testimonials.svelte
@@ -0,0 +1,128 @@
+<script lang="ts">
+
+  import Carousel from './Carousel.svelte';
+  import type { Testimonial } from '$lib/types';
+
+  import aaron from '$lib/assets/imgs/profiles/aaron-fagan.jpeg?enhanced';
+  import austin from '$lib/assets/imgs/profiles/austin-tackaberry.jpeg?enhanced';
+  import gene from '$lib/assets/imgs/profiles/gene-goykhman.jpeg?enhanced';
+  import gil from '$lib/assets/imgs/profiles/gilbert-kwan.jpeg?enhanced';
+  import jake from '$lib/assets/imgs/profiles/jake-alstyne.jpeg?enhanced';
+  import jared from '$lib/assets/imgs/profiles/jared-halpern.jpeg?enhanced';
+  import luispallares from '$lib/assets/imgs/profiles/luis-pallares.jpeg?enhanced';
+  import luisporras from '$lib/assets/imgs/profiles/luis-porras.jpeg?enhanced';
+  import matt from '$lib/assets/imgs/profiles/matt-cantelon.jpeg?enhanced';
+  import sayde from '$lib/assets/imgs/profiles/sayde-deng.jpeg?enhanced';
+  import stephen from '$lib/assets/imgs/profiles/stephenmarie-rhodes.jpeg?enhanced';
+  import stevenStamaria from '$lib/assets/imgs/profiles/steven-santamaria.jpeg?enhanced';
+  import suprada from '$lib/assets/imgs/profiles/suprada-urval.jpeg?enhanced';
+
+    let testimonials: Testimonial[] = [
+    {
+      author: 'Matthew Cantelon',
+      text: `Working with Mauricio for the last year has been the highlight of my career thus far.
+      Mauricio's passion for software development is infectious and his natural ability to lead
+      a team is a sight to behold.`,
+      image: matt
+    },
+    {
+      author: 'Gilbert Kwan',
+      text: `Mau is an excellent developer. He is very detail oriented and hard-working.
+        On many occasions he would drop everything and discuss an issue or find elegant solutions
+        to difficult problems. It was always a pleasure working with him.`,
+      image: gil
+    },
+    {
+      author: 'Sayde Deng',
+      text: `Mau is an awesome mentor who provides constructive feedback, shows deep understanding,
+      and turns ideas into great products. Working with Mau is a joy!`,
+      image: sayde
+    },
+    {
+      author: 'Aaron Fagan',
+      text: `Mauricio is a solid developer and architect, with a solid understanding and vision
+      for the future. He is able to both understand and execute on technical requirements,
+      in addition to being able to communicate those same requirements down to non technical
+      customers as needed.`,
+      image: aaron
+    },
+    {
+      author: 'Stephen Marie-Rhodes',
+      text: `Mau knows all the things (in a good way). He brings momentum and precision to any
+      project. He's just as comfortable leading a team of developers as running
+      solo on a mission-critical project. Mau is also a fantastic teacher.`,
+      image: stephen
+    },
+    {
+      author: 'Suprada Urval',
+      text: `It was great working with Mauricio at HumanAPI.\nHis interest in the landscape of web
+      development, thirst to learn and explore new technologies is infectious and inspiring.\n
+      He models the "disagree but commit" approach when as a team, we make a decision.
+      And then champions that decision.`,
+      image: suprada
+    },
+    {
+      author: 'Steven Santa Maria',
+      text: `I worked with Mauricio at Human API. He delivers the product and -just as
+      important- is responsive. There were times when customers were breathing down our necks and
+      that responsiveness was so helpful to the team. Mauricio will be key contributor to any
+      engineering team!`,
+      image: stevenStamaria
+    },
+    {
+      author: 'Austin Tackaberry',
+      text: `Mauricio was my first software engineering mentor. He was open, encouraging, and easy
+      to get along with. He was always able to come up with simple solutions to complex problems. He
+      has passion for his work, and that passion spreads to others on his team.`,
+      image: austin
+    },
+    {
+      author: 'Gene Goykhman',
+      text: `In addition to his technical talent, Mau is a thoughtful, considerate and dedicated person.
+      He remains consistently engaged and focused on the work at hand while never failing to bring
+      levity and a smile when it is needed the most. Mau is a true pleasure to work with.`,
+      image: gene
+    },
+    {
+      author:'Luis Porras',
+      text: `Working with Mauricio have been a great experience, I have learned a lot from his
+      Node.js experience since I started working with him.\nHe is a curious
+      developer that enjoys learning new programming languages, and technologies. He is capable
+      of solving problems quickly and is always looking to improve things.`,
+      image: luisporras
+    },
+    {
+      author: 'Jake Van Alstyne',
+      text: `Mau is a very highly skilled and charismatic developer who is a real pleasure to
+      work with. He produces timely solutions to difficult problems and works hard to ensure high
+      quality results. In addition to being a critical part of our development team, he possesses
+      admirable character and dedication.`,
+      image: jake
+    },
+    {
+      author: 'Jared Halpern',
+      text: `Mau is the type of guy you want on your team when things get tough. His technical
+      skills were critical to the rapid development of Selfie, and his enthusiasm is contagious.
+      Mau also has a great ability to see things further down the road than just the immediate or
+      next feature.`,
+      image: jared
+    },
+    {
+      author: 'Luis Pallares',
+      text: `He is a developer that you want to have working on your software engineers team.`,
+      image: luispallares
+    }
+  ]
+</script>
+
+  <section class="section-testimonials b-border">
+    <Carousel {testimonials} itemsToShow={2}/>
+  </section>
+
+<style>
+  .section-testimonials {
+    padding: 4.8rem 0;
+    overflow: hidden;
+  }
+
+</style>
---
+++ src/custom.d.ts
@@ -0,0 +1,5 @@
+// custom.d.ts
+declare module '*?enhanced' {
+  const value: any;
+  export = value;
+}
