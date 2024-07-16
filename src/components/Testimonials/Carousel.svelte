8d7ef1e274fab89d2589f7c8c19f96925e7e95bd
Mauricio Mercado <maumercado@gmail.com>
2024-07-16 09:26:11 -0700

    Format/linting (#8)
    Co-authored-by: Mauricio Mercado <mau@maumercado.com>

--- src/components/Testimonials/Carousel.svelte
+++ src/components/Testimonials/Carousel.svelte
@@ -1,115 +1,114 @@
 <script lang="ts">
-  import type { Testimonial } from '$lib/types';
-  import { ChevronBackOutline, ChevronForwardOutline } from 'svelte-ionicons'
-  import { tweened } from 'svelte/motion';
-  import { cubicOut } from 'svelte/easing';
+	import type { Testimonial } from '$lib/types';
+	import { ChevronBackOutline, ChevronForwardOutline } from 'svelte-ionicons';
+	import { tweened } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
 
-  export let testimonials: Testimonial[] = [];
-  export let itemsToShow:number = 1;
+	export let testimonials: Testimonial[] = [];
+	export let itemsToShow: number = 1;
 
-  let currentIndex = 0;
-  let x = tweened(0, { duration: 300, easing: cubicOut });
+	let currentIndex = 0;
+	let x = tweened(0, { duration: 300, easing: cubicOut });
 
-  const showNext = () => {
-    currentIndex = (currentIndex + itemsToShow) % testimonials.length;
-    x.set(-currentIndex * (100 / itemsToShow));
-  };
+	const showNext = () => {
+		currentIndex = (currentIndex + itemsToShow) % testimonials.length;
+		x.set(-currentIndex * (100 / itemsToShow));
+	};
 
-  const showPrev = () => {
-    currentIndex = (currentIndex - itemsToShow + testimonials.length) % testimonials.length;
-    x.set(-currentIndex * (100 / itemsToShow));
-  };
-
+	const showPrev = () => {
+		currentIndex = (currentIndex - itemsToShow + testimonials.length) % testimonials.length;
+		x.set(-currentIndex * (100 / itemsToShow));
+	};
 </script>
 
 <div class="carousel-container">
-  <div class="carousel" style="transform: translateX({$x}%); --itemsToShow: {itemsToShow}">
-    {#each testimonials as testimonial}
-      <figure class="testimonial">
-      <enhanced:img class="testimonial-img" src={testimonial.image} alt={testimonial.author} />
-      <blockquote><p class="testimonial-text">{testimonial.text}</p></blockquote>
-      <cite class="testimonial-author">&mdash; {testimonial.author}</cite>
-      </figure>
-    {/each}
-  </div>
-  <button class="arrow left" on:click={showPrev}>
-    <ChevronBackOutline size={36}/>
-  </button>
-  <button class="arrow right" on:click={showNext}>
-    <ChevronForwardOutline size={36}/>
-  </button>
+	<div class="carousel" style="transform: translateX({$x}%); --itemsToShow: {itemsToShow}">
+		{#each testimonials as testimonial}
+			<figure class="testimonial">
+				<enhanced:img class="testimonial-img" src={testimonial.image} alt={testimonial.author} />
+				<blockquote><p class="testimonial-text">{testimonial.text}</p></blockquote>
+				<cite class="testimonial-author">&mdash; {testimonial.author}</cite>
+			</figure>
+		{/each}
+	</div>
+	<button class="arrow left" on:click={showPrev}>
+		<ChevronBackOutline size={36} />
+	</button>
+	<button class="arrow right" on:click={showNext}>
+		<ChevronForwardOutline size={36} />
+	</button>
 </div>
 
 <style>
-  .carousel-container {
-    position: relative;
-    width: 100%;
-    overflow: hidden;
-  }
+	.carousel-container {
+		position: relative;
+		width: 100%;
+		overflow: hidden;
+	}
 
-  .carousel {
-    display: grid;
-    grid-template-columns: repeat(auto-fill,minmax(calc(100% / var(--itemsToShow)),1fr));
+	.carousel {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(calc(100% / var(--itemsToShow)), 1fr));
 
-    grid-auto-flow: column;
-    grid-auto-columns: minmax(calc(100% / var(--itemsToShow)),1fr);
+		grid-auto-flow: column;
+		grid-auto-columns: minmax(calc(100% / var(--itemsToShow)), 1fr);
 
-    transition: transform 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
-  }
+		transition: transform 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+	}
 
-  .testimonial {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    transition: all 1s ease;
-    padding: 2.4rem 4.8rem;
-  }
+	.testimonial {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		transition: all 1s ease;
+		padding: 2.4rem 4.8rem;
+	}
 
-  .testimonial-img {
-    border-radius: 50%;
-    width: 8rem;
-    height: 8rem;
-  }
+	.testimonial-img {
+		border-radius: 50%;
+		width: 8rem;
+		height: 8rem;
+	}
 
-  .testimonial-text {
-    padding-top: 1.2rem;
-    font-size: 1.6rem;
-    line-height: 1.5;
-  }
+	.testimonial-text {
+		padding-top: 1.2rem;
+		font-size: 1.6rem;
+		line-height: 1.5;
+	}
 
-  .testimonial-author {
-    font-size: 1.6rem;
-    padding: 1.4rem 0;
-  }
+	.testimonial-author {
+		font-size: 1.6rem;
+		padding: 1.4rem 0;
+	}
 
-.arrow {
-    position: absolute;
-    top: 50%;
-    width: 6rem;
-    height: 6rem;
-    cursor: pointer;
-    color: var(--theme-font-default);
-    opacity: 1;
-    transition: all 0.3s ease;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+	.arrow {
+		position: absolute;
+		top: 50%;
+		width: 6rem;
+		height: 6rem;
+		cursor: pointer;
+		color: var(--theme-font-default);
+		opacity: 1;
+		transition: all 0.3s ease;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
 
-  .arrow.left {
-    left: 0;
-    z-index: 1;
-  }
+	.arrow.left {
+		left: 0;
+		z-index: 1;
+	}
 
-  .arrow.right {
-    right: 0;
-  }
+	.arrow.right {
+		right: 0;
+	}
 
-  .carousel-container:hover .arrow {
-    opacity: 1;
-  }
+	.carousel-container:hover .arrow {
+		opacity: 1;
+	}
 
-  .arrow:hover {
-    transform: scale(1.5);
-  }
+	.arrow:hover {
+		transform: scale(1.5);
+	}
 </style>
