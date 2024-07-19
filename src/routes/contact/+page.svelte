426353cef60489f5fcdb4ebb8c95c8599e8d95f9
Mauricio Mercado <mau@maumercado.com>
2024-07-18 17:48:32 -0700

    Set default styles to input, text area and button, and set some styles for the form

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,38 @@
+
+<form class="contact-form" on:submit|preventDefault={() => {}} method="POST">
+  <div class='form-group'>
+    <label for="name">Name</label>
+    <input required type="text" name="name" placeholder="Full Name"/>
+  </div>
+  <div class='form-group'>
+    <label for="email">Email</label>
+    <input required type="email" name="email" placeholder="someone@example.com"/>
+  </div>
+  <div class='form-group form-group--full-width'>
+    <label for="message">Message</label>
+    <textarea required name="message" rows="6" placeholder="What can I help you with..."/>
+  </div>
+  <button class="btn-primary" type="submit">Send Message</button>
+</form>
+
+
+<style>
+
+  .contact-form {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 2.4rem;
+    width: calc(var(--max-width)/2);
+    justify-content: stretch;
+  }
+
+  .form-group {
+    display: grid;
+    gap: 0.8rem;
+  }
+
+  .form-group--full-width {
+    grid-column: 1 / -1;
+  }
+
+</style>
