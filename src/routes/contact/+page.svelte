023b77b3d93449a35ebf40ecd0a99c81179322e7
Mauricio Mercado <mau@maumercado.com>
2024-07-22 13:09:58 -0700

    refactor: simplify import statement
    Remove unnecessary import of SuperDebug from 'sveltekit-superforms'

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import SuperDebug, { superForm } from 'sveltekit-superforms';
+	import { superForm } from 'sveltekit-superforms';
   import { zodClient } from 'sveltekit-superforms/adapters'
 
 	import { ContactSchema } from '$routes/contact/schema';
