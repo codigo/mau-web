2390a08ef55f2a685cb5e095a7c44e79c91791e2
Mauricio Mercado <mau@maumercado.com>
2024-07-31 16:41:18 -0700

    feat: filter and optimize posts display
    Apply publish filter to posts queries for more relevant content.
    Add lazy loading to post images for performance improvement.
    Enhance table headers with bold font for better readability.
    Refactor CSS grid for posts layout.

--- src/routes/journal/Posts.svelte
+++ src/routes/journal/Posts.svelte
@@ -19,15 +19,18 @@
 						tabindex={idx}
 						on:click={() => onClick(`/journal/${post.slug}`)}
 					>
-						<img class="post-image" alt={post.img_url_alt} src={`${post.img_url}&w=400`} />
-						<h3>{post.title}</h3>
+						<img
+							class="post-image"
+							alt={post.img_url_alt}
+							src={`${post.img_url}&w=400`}
+							loading="lazy"
+						/>
 						<ul class="post-tags">
 							{#each post.tags.split(',') as tag}
 								<li class="post-tag-item">#{tag.trim()}</li>
 							{/each}
 						</ul>
-
-						<p>{post.summary}</p>
+						<h3>{post.title}</h3>
 					</button>
 				</article>
 			</li>
@@ -42,9 +45,8 @@
 
 	.posts-list {
 		display: grid;
-		grid-auto-flow: column;
-		column-gap: 2rem;
 		grid-template-columns: repeat(4, 1fr);
+		gap: 2rem;
 	}
 
 	.post-item {
@@ -92,7 +94,8 @@
 
 	.post-image {
 		border-radius: var(--theme-border-radius-default);
+		object-fit: cover;
 		width: 100%;
-		/* box-shadow: 6px 6px 8px 3px rgba(0, 0, 0, 0.3); */
+		height: 18rem;
 	}
 </style>
