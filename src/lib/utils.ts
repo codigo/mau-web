153e447039d001747ab519476157540fbfbb5028
Mauricio Mercado <maumercado@gmail.com>
2024-09-27 17:55:46 -0700

    feat: add utility functions and tech stack constants (#54)
    * ✨ feat: add utility functions and tech stack constants
    
    Implement utility functions to split arrays into chunks and 
    duplicate objects in arrays. Add a new tech stack constants 
    file that contains various tech icons and their categories 
    for improved organization and easy access within the codebase.
    
    * ✨ feat: create script to generate "About Me" file
    
    Add a new script to generate an "About Me" text file by 
    reading experiences from markdown files and tech stack from 
    TypeScript constants. Implement directory checks, file 
    reading, and content formatting to build a comprehensive 
    summary. This improves documentation and provides a 
    clearer overview of personal experiences and skills.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,3 +6,24 @@ export function formatDate(date: string, dateStyle: DateStyle = 'medium', locale
 	const dateFormatter = new Intl.DateTimeFormat(locales, { dateStyle });
 	return dateFormatter.format(dateToFormat);
 }
+
+export function splitArrayIntoChunks<T>(arr: T[], numChunks: number): T[][] {
+	const chunks: T[][] = [];
+	const n = arr.length;
+	const chunkSize = Math.floor(n / numChunks);
+	const remainder = n % numChunks;
+
+	for (let i = 0; i < numChunks; i++) {
+		let start = i * chunkSize;
+		let end = start + chunkSize;
+		chunks.push(arr.slice(start, end));
+	}
+
+	// Add the remaining elements to the last chunk
+	chunks[numChunks - 1] = chunks[numChunks - 1].concat(arr.slice(chunkSize * numChunks));
+	return chunks;
+}
+
+export function duplicateObjectsInArray<T extends { duplicate?: boolean }>(arr: T[][]): T[][] {
+	return arr.map((chunk) => [...chunk, ...chunk.map((item) => ({ ...item, duplicate: true }))]);
+}
